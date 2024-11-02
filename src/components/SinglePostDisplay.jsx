1e7b9e6c43fe7e6b48cd112bf740416e5dc4d96c
Marina Muñoz <marinacerejidoo@gmail.com>
2024-11-02 16:45:30 +0100

    Users can now like posts

--- src/components/SinglePostDisplay.jsx
+++ src/components/SinglePostDisplay.jsx
@@ -4,6 +4,35 @@ const SinglePostDisplay = ({ post }) => {
   const userId = localStorage.getItem("userId");
   const [isVisible, setIsVisible] = useState(true); //for when deleting the post
 
+  const [likes, setLikes] = useState(post.likes.length); // general like count
+  const [isLiked, setIsLiked] = useState(post.likes.includes(userId)); // did the logged in user like it?
+
+  //this is a like and dislike function
+  const handleLike = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/posts/${post._id}/like`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({ userId }),
+        }
+      );
+
+      if (response.ok) {
+        setIsLiked(!isLiked);
+        setLikes(isLiked ? likes - 1 : likes + 1);
+      } else {
+        console.error("Failed to like/unlike post");
+      }
+    } catch (error) {
+      console.error("Error liking/unliking post:", error);
+    }
+  };
+
   const handleDelete = async () => {
     try {
       const response = await fetch(
@@ -32,6 +61,7 @@ const SinglePostDisplay = ({ post }) => {
       <h3>{post.userId.username}</h3> <p>{post.content}</p>
       {post.image && <img src={post.image} alt="Post" />}
       <div>Likes: {post.likes.length}</div>
+      <button onClick={handleLike}>{isLiked ? "Unlike" : "Like"}</button>
       {userId === post.userId._id && (
         <button onClick={handleDelete} style={{ color: "red" }}>
           Delete
